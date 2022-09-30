dd664ad42c32d04557f4113fbee49a05aa2594fa
Saulo <saulomcm@gmail.com>
2022-09-30 10:50:18 -0300

    Adiciona POUI ao projeto

---
+++ src/poui/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { RouterModule } from '@angular/router';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PoModule,
+    RouterModule.forRoot([]),
+    PoTemplatesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
