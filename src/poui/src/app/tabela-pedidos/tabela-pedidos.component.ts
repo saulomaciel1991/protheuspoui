88d94f4ce2b267d78a4cc6f9046e91a0b0c98502
caroltavares1 <caroltavares.ts@gmail.com>
2022-10-06 08:53:17 -0300

    adicionando componente tabela-pedidos

---
+++ src/poui/src/app/tabela-pedidos/tabela-pedidos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-pedidos',
+  templateUrl: './tabela-pedidos.component.html',
+  styleUrls: ['./tabela-pedidos.component.scss']
+})
+export class TabelaPedidosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
