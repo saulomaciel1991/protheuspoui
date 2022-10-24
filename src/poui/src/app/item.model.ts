c3ff8653ca6f51b3820a23baf43cb71d41064579
caroltavares1 <caroltavares.ts@gmail.com>
2022-10-24 09:17:37 -0300

    adicionando os modelos de pedido e item

---
+++ src/poui/src/app/item.model.ts
@@ -0,0 +1,9 @@
+export class Item{
+    public TES: string
+    public qtd: number
+    public preco_unitario: number
+    public preco_total: number
+    public item: string
+    public produto: string
+   // public descricao: string
+}
---
+++ src/poui/src/app/pedido.model.ts
@@ -0,0 +1,13 @@
+import { Item } from "./item.model"
+
+export class Pedido {
+    public loja: string
+    public natureza: string
+    public status: string
+    public tipoPed: string
+    public cliente : string
+    public numero : string
+    public condPag : string
+    public itens: Item[]
+    public nomeCliente : string
+}
